a8d9523381c5c2ca52ea21367de6e8b5dfc93513
kyranjamie <kyran.burraston@gmail.com>
2020-06-22 11:26:40 +0200

    feat: persist keys on disk, closes #145
    feat: add test set up

--- app/store/keys/keys.reducer.ts
+++ app/store/keys/keys.reducer.ts
@@ -1,30 +1,53 @@
 import { createReducer, createSelector } from '@reduxjs/toolkit';
 import log from 'electron-log';
 
-import { RootState } from '../reducers';
-import { persistMnemonicSafe, removeMnemonic, persistMnemonic } from './keys.actions';
+import { RootState } from '..';
+import { setPasswordSuccess } from './keys.actions';
+import { encryptMnemonic } from 'blockstack';
+import {
+  persistMnemonicSafe,
+  persistMnemonic,
+  addData,
+  attemptWalletDecryptFailed,
+  attemptWalletDecrypt,
+} from './keys.actions';
 
 export interface KeysState {
   mnemonic: string | null;
+  decrypting: boolean;
+  salt?: string;
+  decryptionError?: string;
+  encryptMnemonic?: string;
 }
 
 const initialState: Readonly<KeysState> = Object.freeze({
   mnemonic: null,
+  decrypting: false,
 });
 
-export const keyReducer = createReducer(initialState, builder =>
-  builder
-    .addCase(persistMnemonicSafe, (state, action) => {
-      if (state.mnemonic !== null) {
-        log.warn('generateMnemonicSafe failed. Tried to create mnemonic when one already exists.');
-        return state;
-      }
-      return { mnemonic: action.payload };
-    })
-    .addCase(persistMnemonic, (_state, action) => ({ mnemonic: action.payload }))
-    .addCase(removeMnemonic, () => ({ mnemonic: null }))
-);
+export const createKeysReducer = (keys: Partial<KeysState> = {}) =>
+  createReducer({ ...initialState, ...keys }, builder =>
+    builder
+      .addCase(persistMnemonicSafe, (state, action) => {
+        if (state.mnemonic !== null) {
+          log.warn(
+            'generateMnemonicSafe failed. Tried to create mnemonic when one already exists.'
+          );
+          return state;
+        }
+        return { ...state, mnemonic: action.payload };
+      })
+      .addCase(persistMnemonic, (state, action) => ({ ...state, mnemonic: action.payload }))
+      .addCase(addData, (state, action) => ({ ...state, ...(action.payload as KeysState) }))
+      .addCase(setPasswordSuccess, (state, action) => ({ ...state, ...action.payload }))
+      .addCase(attemptWalletDecrypt, state => ({ ...state, decrypting: true }))
+      .addCase(attemptWalletDecryptFailed, (state, action) => ({
+        ...state,
+        decrypting: false,
+        decryptionError: action.payload.decryptionError,
+      }))
+  );
 
-const selectKeysSlice = (state: RootState) => state.keys;
+export const selectKeysSlice = (state: RootState) => state.keys;
 
 export const selectMnemonic = createSelector(selectKeysSlice, state => state.mnemonic);
